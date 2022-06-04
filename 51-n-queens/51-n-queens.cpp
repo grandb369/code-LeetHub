7f39e4395127873a40a047727d968b87a4c7c793
Yip Sum LAM(Anthony LAM) <42032578+grandb369@users.noreply.github.com>
2022-06-04 15:59:36 +0800

    Time: 19 ms (32.27%), Space: 11.9 MB (25.03%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
-    void dfs(vector<vector<string>>&out, vector<string> temp, set<int> x, set<int>dia, set<int> ndia ,int r, int n)
+    bool check(vector<string>&temp, int r, int c, int n)
+    {
+        for(int i=0;i<=r;i++)
+        {
+            if(temp[i][c]=='Q')return false;
+        }
+        int x=r;
+        int y=c;
+        while (x>=0 && y<n)
+        {
+            if(temp[x][y]=='Q')return false;
+            x--;
+            y++;
+        }
+        x=r;
+        y=c;
+        while(x>=0 && y>=0)
+        {
+            if(temp[x][y]=='Q')return false;
+            x--;
+            y--;
+        }
+        return true;
+    }
+    void dfs(vector<vector<string>>&out, vector<string> temp, int r, int n)
     {
         if(r==n)out.push_back(temp);
         else
         {
             for(int i=0;i<n;i++)
             {
-                if (x.find(i)!=x.end())continue;
-                if (dia.find(i-r)!=dia.end())continue;
-                if (ndia.find(i+r)!=ndia.end())continue;
-                x.insert(i);
-                dia.insert(i-r);
-                ndia.insert(i+r);
-                temp.push_back(string(i,'.')+'Q'+string(n-1-i,'.'));
-                dfs(out,temp,x,dia,ndia,r+1,n);
-                temp.pop_back();
-                x.erase(i);
-                dia.erase(i-r);
-                ndia.erase(i+r);
+                if(check(temp,r,i,n))
+                {
+                    temp[r][i]='Q';
+                    dfs(out,temp,r+1,n);
+                    temp[r][i]='.';
+                }
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>out;
-        dfs(out,vector<string>{},set<int>{},set<int>{},set<int>{},0,n);
+        dfs(out,vector<string>(n,string(n,'.')),0,n);
         return out;
     }
 };
