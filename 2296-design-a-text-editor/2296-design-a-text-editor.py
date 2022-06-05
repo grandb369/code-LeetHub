9384acab21097061d2e7feb07d637290829b14b2
Yip Sum LAM(Anthony LAM) <42032578+grandb369@users.noreply.github.com>
2022-06-05 22:45:32 +0800

    Time: 5883 ms (20.00%), Space: 29.7 MB (20.00%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.py
+++ 2296-design-a-text-editor/2296-design-a-text-editor.py
@@ -1,49 +1,26 @@
 class TextEditor:
 
     def __init__(self):
-        self.stack1=[]
-        self.stack2=[]
+        self.s = ''
+        self.cursor = 0
 
     def addText(self, text: str) -> None:
-        for i in text:
-            self.stack1.append(i)
+        self.s = self.s[:self.cursor] + text + self.s[self.cursor:]
+        self.cursor += len(text)
 
     def deleteText(self, k: int) -> int:
-        c=0
-        while self.stack1 and k:
-            self.stack1.pop()
-            k-=1
-            c+=1
-        return c
+        new_cursor = max(0, self.cursor - k)
+        noOfChars = k if self.cursor - k >= 0 else self.cursor
+        self.s = self.s[:new_cursor] + self.s[self.cursor:]
+        self.cursor = new_cursor
+        return noOfChars
 
     def cursorLeft(self, k: int) -> str:
-        while self.stack1 and k:
-            self.stack2.append(self.stack1.pop())
-            k-=1
-        n=len(self.stack1)
-        out=''
-        for i in range(n-1,n-11,-1):
-            if i<0:
-                break
-            out=self.stack1[i]+out
-        return out
+        self.cursor = max(0, self.cursor - k)
+        start = max(0, self.cursor-10)
+        return self.s[start:self.cursor]
 
     def cursorRight(self, k: int) -> str:
-        while self.stack2 and k:
-            self.stack1.append(self.stack2.pop())
-            k-=1
-        n=len(self.stack1)
-        out=''
-        for i in range(n-1,n-11,-1):
-            if i<0:
-                break
-            out=self.stack1[i]+out
-        return out
-
-
-# Your TextEditor object will be instantiated and called as such:
-# obj = TextEditor()
-# obj.addText(text)
-# param_2 = obj.deleteText(k)
-# param_3 = obj.cursorLeft(k)
-# param_4 = obj.cursorRight(k)
+        self.cursor = min(len(self.s), self.cursor + k)
+        start = max(0, self.cursor - 10)
+        return self.s[start:self.cursor]
