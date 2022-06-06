9393636cf079238e8a24226d4dcc6fa66f128955
Yip Sum LAM(Anthony LAM) <42032578+grandb369@users.noreply.github.com>
2022-06-06 17:59:42 +0800

    Time: 3 ms (89.46%), Space: 9.6 MB (52.68%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int n=mat.size();
+        int m=mat[0].size();
         int r=0;
-        int c=matrix[0].size()-1;
-        int n=matrix.size();
-        while (r<n && c>=0)
+        int c=m-1;
+        while(r<n && c>=0)
         {
-            if(matrix[r][c]==target)return true;
-            else if (matrix[r][c]>target)c--;
+            if(mat[r][c]==target)return true;
+            else if (mat[r][c]>target)c--;
             else r++;
         }
         return false;
+        
     }
 };
