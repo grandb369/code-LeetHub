0b57623c85535e054bf67a66bf049ab019a478fe
Yip Sum LAM(Anthony LAM) <42032578+grandb369@users.noreply.github.com>
2022-06-19 12:54:55 +0800

    Time: 3987 ms (66.67%), Space: 20.3 MB (100.00%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.py
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.py
@@ -1,12 +1,13 @@
-class Solution:    
-    def sellingWood(self, m, n, prices):
-        dp = [[0] * (n + 1) for i in range(m+1)]
-        for w, h, p in prices:
-            dp[w][h] = p
-        for w in range(1, m + 1):
-            for h in range(1, n + 1):
-                for a in range(1, w // 2 + 1):
-                    dp[w][h] = max(dp[w][h], dp[a][h] + dp[w - a][h])
-                for a in range(1, h // 2 + 1):
-                    dp[w][h] = max(dp[w][h], dp[w][a] + dp[w][h - a])
-        return dp[m][n]
+class Solution:
+    def sellingWood(self, m: int, n: int, pr: List[List[int]]) -> int:
+        dp=[[0for i in range(n+1)]for j in range(m+1)]
+        for r,c,p in pr:
+            dp[r][c]=p
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                for r in range(1,i//2+1):
+                    dp[i][j]=max(dp[i][j],dp[r][j]+dp[i-r][j])
+                for c in range(1,j//2+1):
+                    dp[i][j]=max(dp[i][j],dp[i][c]+dp[i][j-c])
+        return dp[-1][-1]
+                    
