3ed7df25d002963fa8d2ddad17a610a210be044d
Yip Sum LAM(Anthony LAM) <42032578+grandb369@users.noreply.github.com>
2022-06-20 18:08:25 +0800

    Time: 139 ms (25.80%), Space: 14.5 MB (26.04%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.py
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.py
@@ -2,23 +2,25 @@ import threading
 class FooBar:
     def __init__(self, n):
         self.n = n
-        self.lock_foo = threading.Lock()
-        self.lock_bar = threading.Lock()
-        self.lock_bar.acquire()
+        self.lock_foo = threading.Event()
+        self.lock_bar = threading.Event()
+        self.lock_foo.set()
 
     def foo(self, printFoo: 'Callable[[], None]') -> None:
         
         for i in range(self.n):
-            self.lock_foo.acquire()
+            self.lock_foo.wait()
             # printFoo() outputs "foo". Do not change or remove this line.
             printFoo()
-            self.lock_bar.release()
+            self.lock_foo.clear()
+            self.lock_bar.set()
 
 
     def bar(self, printBar: 'Callable[[], None]') -> None:
         for i in range(self.n):
-            self.lock_bar.acquire()    
+            self.lock_bar.wait()    
             # printBar() outputs "bar". Do not change or remove this line.
             printBar()
-            self.lock_foo.release()
+            self.lock_bar.clear()
+            self.lock_foo.set()
             
