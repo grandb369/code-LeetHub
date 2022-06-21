a24f4659d5b7ab63097c97336179847efa06b7e0
Yip Sum LAM(Anthony LAM) <42032578+grandb369@users.noreply.github.com>
2022-06-21 17:44:00 +0800

    Time: 41 ms (95.95%), Space: 14.4 MB (55.14%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.cpp
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -1,28 +1,25 @@
-class Foo {
-public:
-    promise<void>p1;
-    promise<void>p2;
-    Foo() {
-        
-    }
+import threading
+class Foo:
+    def __init__(self):
+        self.p1=threading.Event()
+        self.p2=threading.Event()
 
-    void first(function<void()> printFirst) {
-        
-        // printFirst() outputs "first". Do not change or remove this line.
-        printFirst();
-        p1.set_value();
-    }
 
-    void second(function<void()> printSecond) {
-        p1.get_future().wait();
-        // printSecond() outputs "second". Do not change or remove this line.
-        printSecond();
-        p2.set_value();
-    }
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.p1.set()
 
-    void third(function<void()> printThird) {
-        p2.get_future().wait();
-        // printThird() outputs "third". Do not change or remove this line.
-        printThird();
-    }
-};
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.p1.wait()
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+        self.p1.clear()
+        self.p2.set()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self.p2.wait()
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
+        self.p2.clear()
