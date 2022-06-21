6f1d44c41e589957f2f272f3267424c15d3e2161
Yip Sum LAM(Anthony LAM) <42032578+grandb369@users.noreply.github.com>
2022-06-21 19:19:30 +0800

    Time: 64 ms (72.46%), Space: 15.1 MB (37.29%) - LeetHub

--- 1116-print-zero-even-odd/1116-print-zero-even-odd.py
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.py
@@ -1,51 +1,35 @@
-class ZeroEvenOdd {
-private:
-    int n;
-    mutex m0,mo,me;
-
-public:
-    ZeroEvenOdd(int n) {
-        this->n = n;
-        me.lock();
-        mo.lock();
-    }
-
-    // printNumber(x) outputs "x", where x is an integer.
-    void zero(function<void(int)> printNumber) {
-        for(int i=1;i<this->n+1;i++)
-        {
-            m0.lock();
-            printNumber(0);
-            if (i%2)
-            {
-                mo.unlock();
-            }
-            else
-            {
-                me.unlock();
-            }
-        }
-            
+from threading import Lock, Event
+class ZeroEvenOdd:
+    def __init__(self, n):
+        self.n = n
+        self.m0=Lock()
+        self.m1=Lock()
+        self.m2=Lock()
+        self.m1.acquire()
+        self.m2.acquire()
         
-    }
-
-    void even(function<void(int)> printNumber) {
-        for(int i=1;i<this->n+1;i++)
-        {
-            if(i%2)continue;
-            me.lock();
-            printNumber(i);
-            m0.unlock();
-        }
-    }
-
-    void odd(function<void(int)> printNumber) {
-        for(int i=1;i<this->n+1;i++)
-        {
-            if(i%2==0)continue;
-            mo.lock();
-            printNumber(i);
-            m0.unlock();
-        }
-    }
-};
+        
+        
+	# printNumber(x) outputs "x", where x is an integer.
+    def zero(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(1, 1+self.n):
+            self.m0.acquire()
+            printNumber(0)
+            if i%2:
+                self.m1.release()
+            else:
+                self.m2.release()
+        
+    def even(self, printNumber: 'Callable[[int], None]') -> None:
+        for j in range(2,1+self.n,2):
+            self.m2.acquire()
+            printNumber(j)
+            self.m0.release()
+        
+        
+    def odd(self, printNumber: 'Callable[[int], None]') -> None:
+        for x in range(1,1+self.n,2):
+            self.m1.acquire()
+            printNumber(x)
+            self.m0.release()
+        
