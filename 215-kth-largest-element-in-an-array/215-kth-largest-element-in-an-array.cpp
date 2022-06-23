5e536cc2a5dc5a1ccd8e9b4914852fc6fef5450e
Yip Sum LAM(Anthony LAM) <42032578+grandb369@users.noreply.github.com>
2022-06-23 15:28:04 +0800

    Time: 9 ms (76.86%), Space: 10.4 MB (9.56%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        partial_sort(nums.rbegin(),nums.rbegin()+k,nums.rend(),std::greater{});
-        return nums[nums.size()-k];
+        priority_queue<int>temp;
+        for(int i:nums)temp.push(i);
+        int out=nums[0];
+        while(k>0)
+        {
+            out=temp.top();
+            temp.pop();
+            k--;
+        }
+        return out;
     }
 };
