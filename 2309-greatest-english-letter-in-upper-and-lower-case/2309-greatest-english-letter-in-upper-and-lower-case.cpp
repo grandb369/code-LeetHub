0234776825988b70d24897a0e7e25334d3b6dbcf
Yip Sum LAM(Anthony LAM) <42032578+grandb369@users.noreply.github.com>
2022-06-23 16:58:28 +0800

    Time: 20 ms (13.70%), Space: 6.9 MB (20.19%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     string greatestLetter(string s) {
-        char out='\0';
+        string out="";
+        char val='\0';
         set<char> temp;
         for(char i:s)
         {
             if(isupper(i) && temp.find(tolower(i))!=temp.end())
             {
-                if(!out)out=i;
-                else out=max(out,i);
+                if(!val)val=i;
+                else val=max(val,i);
             }
             else if(islower(i) && temp.find(toupper(i))!=temp.end())
             {
                 i=toupper(i);
-                if(!out)out=i;
-                else out=max(out,i);
+                if(!val)val=i;
+                else val=max(val,i);
             }
             temp.insert(i);
         }
-        if(out=='\0')return "";
-        string ans="";
-        ans+=out;
-        return ans;
+        if(val)out+=val;
+        return out;
     }
 };
