fd8bb5a66e98a48cf29aa6e9828c189e8c659591
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2022-06-26 17:05:35 +0800

    Time: 49 ms (73.54%), Space: 20 MB (11.32%) - LeetHub

--- 707-design-linked-list/707-design-linked-list.cpp
+++ 707-design-linked-list/707-design-linked-list.cpp
@@ -1,20 +1,25 @@
 struct node{
     int val;
     node* next;
+    node* pre;
     node():val(0),next(nullptr){};
     node(int v):val(v),next(nullptr){};
 };
 class MyLinkedList {
 public:
     node* head;
+    node* tail;
     int n;
     MyLinkedList() {
         head=new node();
+        tail=new node();
+        head->next=tail;
+        tail->pre=head;
         n=0;
     }
     
     int get(int index) {
-        if (index>=n)return -1;
+        if(index>=n)return -1;
         node* cur=head->next;
         while(index>0)
         {
@@ -22,25 +27,25 @@ public:
             index--;
         }
         return cur->val;
-        
     }
     
     void addAtHead(int val) {
-        node* cur=new node(val);
         node* nex=head->next;
+        node* cur=new node(val);
         head->next=cur;
         cur->next=nex;
+        nex->pre=cur;
+        cur->pre=head;
         n++;
     }
     
     void addAtTail(int val) {
-        node* nex=new node(val);
-        node* cur=head;
-        while(cur->next!=nullptr)
-        {
-            cur=cur->next;
-        }
-        cur->next=nex;
+        node* pre=tail->pre;
+        node* cur=new node(val);
+        pre->next=cur;
+        cur->next=tail;
+        tail->pre=cur;
+        cur->pre=pre;
         n++;
     }
     
@@ -50,28 +55,32 @@ public:
             node* pre=head;
             while(index>0)
             {
-                index--;
                 pre=pre->next;
+                index--;
             }
             node* nex=pre->next;
-            node* cur = new node(val);
+            node* cur=new node(val);
             pre->next=cur;
             cur->next=nex;
+            nex->pre=cur;
+            cur->pre=pre;
             n++;
         }
+
     }
     
     void deleteAtIndex(int index) {
         if(index<n)
         {
             node* pre=head;
-            while (index>0)
+            while(index>0)
             {
-                index--;
                 pre=pre->next;
+                index--;
             }
             node* nex=pre->next->next;
             pre->next=nex;
+            nex->pre=pre;
             n--;
         }
     }
