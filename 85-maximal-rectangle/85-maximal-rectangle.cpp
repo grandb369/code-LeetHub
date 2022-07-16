17f3e7ce65fdf2926fcbe80f124aedf1841988a0
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2022-07-16 16:14:10 +0800

    Time: 72 ms (54.72%), Space: 12.2 MB (95.12%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -4,25 +4,25 @@ public:
         int r,c;
         int n=mat.size();
         int m=mat[0].size();
+        vector<int>stack;
         vector<int>cur(m+2,0);
         vector<int>pre(m+2,0);
         int out=0;
         for(r=0;r<n;r++)
         {
-            for(c=0;c<m;c++)
-            {
-                if(mat[r][c]=='0')
-                {
-                    cur[c+1]=0;
-                }
-                else
-                {
-                    cur[c+1]=pre[c+1]+1;
-                }
-            }
-            vector<int>stack;
             for(c=0;c<m+2;c++)
             {
+                if(c<m)
+                {
+                    if(mat[r][c]=='0')
+                    {
+                        cur[c+1]=0;
+                    }
+                    else
+                    {
+                        cur[c+1]=pre[c+1]+1;
+                    }
+                }     
                 while(stack.size()>0 && cur[stack.back()]>cur[c])
                 {
                     int index=stack.back();
@@ -32,6 +32,7 @@ public:
                 stack.push_back(c);
             }
             pre=cur;
+            stack.clear();
         }
         return out;
     }
