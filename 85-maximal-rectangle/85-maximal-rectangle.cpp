4fcdbc26fbadcae328f3e7259c00a176b8af0acb
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2022-07-16 16:10:04 +0800

    Time: 41 ms (97.16%), Space: 13.1 MB (85.41%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& mat) {
+        int r,c;
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<int>cur(m+2,0);
+        vector<int>pre(m+2,0);
+        int out=0;
+        for(r=0;r<n;r++)
+        {
+            for(c=0;c<m;c++)
+            {
+                if(mat[r][c]=='0')
+                {
+                    cur[c+1]=0;
+                }
+                else
+                {
+                    cur[c+1]=pre[c+1]+1;
+                }
+            }
+            vector<int>stack;
+            for(c=0;c<m+2;c++)
+            {
+                while(stack.size()>0 && cur[stack.back()]>cur[c])
+                {
+                    int index=stack.back();
+                    stack.pop_back();
+                    out=max(out,cur[index]*(c-stack.back()-1));
+                }
+                stack.push_back(c);
+            }
+            pre=cur;
+        }
+        return out;
+    }
+};
