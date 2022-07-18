007ec58b3d7c28c9df4648b2c636f5ec5b58d994
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2022-07-19 02:24:28 +0800

    Time: 37 ms (34.53%), Space: 16.7 MB (41.48%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,60 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& nums, int t) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>stack;
-        vector<vector<int>>out;
-        vector<int>ind;
-        vector<int>val;
-        int n=nums.size();
-        int i,j;
-        int c=0;
-        for(int v:nums)
+    void backtrack(vector<vector<int>>&out, vector<int>nums, vector<int>temp, int v, int t)
+    {
+        if(v==t)
         {
-            vector<int>temp{v};
-            if(v==t)
-            {
-                out.push_back(temp);
-                continue;
-            }
-            stack.push_back(temp);
-            val.push_back(v);
-            ind.push_back(c);
-            c++;
+            out.push_back(temp);
         }
-        while(stack.size()>0)
+        else
         {
-            vector<vector<int>>stack2;
-            vector<int>val2;
-            vector<int>ind2;
-            vector<int>temp;
-            for(i=0;i<stack.size();i++)
+            while(nums.size()>0)
             {
-                vector<int>cur=stack[i];
-                int v=val[i];
-                int index=ind[i];
-                for(j=index;j<n;j++)
+                int val=nums.back();
+                if(val+v<=t)
                 {
-                    if(v+nums[j]==t)
-                    {
-                        cur.push_back(nums[j]);
-                        out.push_back(cur);
-                        break;
-                    }
-                    else if(v+nums[j]<t)
-                    {
-                        vector<int>nex=cur;
-                        nex.push_back(nums[j]);
-                        stack2.push_back(nex);
-                        val2.push_back(v+nums[j]);
-                        ind2.push_back(j);
-                    }
+                    temp.push_back(val);
+                    backtrack(out,nums,temp,v+val,t);
+                    temp.pop_back();
                 }
+                nums.pop_back();
             }
-            stack=stack2;
-            val=val2;
-            ind=ind2;
         }
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int t) {
+        vector<vector<int>>out;
+        backtrack(out,nums,vector<int>{},0,t);
         return out;
     }
 };
