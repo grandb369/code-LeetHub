21d89fa288ace069df5f866c6d16f2f08f7c1c26
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2022-07-18 16:59:05 +0800

    Time: 156 ms (39.70%), Space: 41.5 MB (72.66%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -3,18 +3,16 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
         int i;
-        for(i=1;i<nums.size();i++)
-        {
-            nums[i]+=nums[i-1];
-        }
         unordered_map<int,int>temp;
         temp[0]=1;
         int out=0;
-        for(i=0;i<n;i++)
+        for(i=0;i<nums.size();i++)
         {
+            if(i>0)nums[i]+=nums[i-1];
             if(temp.find(nums[i]-k)!=temp.end())out+=temp[nums[i]-k];
             temp[nums[i]]++;
         }
+        
         return out;
     }
 };
