671c81573c05b9bacef2c64eb2cdbf9b78bb4cc1
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2022-07-19 21:48:51 +0800

    Time: 267 ms (21.86%), Space: 11.8 MB (9.10%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int numTeams(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>more(n,0);
-        vector<int>less(n,0);
-        int out=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                if(nums[i]>nums[j])
-                {
-                    less[i]+=1;
-                    out+=less[j];
-                }
-                if(nums[i]<nums[j])
-                {
-                    more[i]+=1;
-                    out+=more[j];
+    int numTeams(vector<int>& v) {
+        int n = v.size(), ans = 0;
+        vector dp(n, vector<int>(3)), dp2(n, vector<int>(3));
+        for (int i = 0; i < n; ++i) {
+            dp[i][0] = dp2[i][0] = 1;
+            for (int j = 0; j < i; ++j) {
+                if (v[j] < v[i]) {
+                    dp[i][1] += dp[j][0];
+                    dp[i][2] += dp[j][1];
+                } else {
+                    dp2[i][1] += dp2[j][0];
+                    dp2[i][2] += dp2[j][1];
                 }
             }
+            ans += dp[i][2] + dp2[i][2];
         }
-        return out;
+        return ans;
     }
 };
