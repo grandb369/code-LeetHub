4ce12d3eb4b9710c6475ce8c1b23c492f6cecd92
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2022-07-20 12:25:38 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (27.64%) - LeetHub

--- 829-consecutive-numbers-sum/829-consecutive-numbers-sum.cpp
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int consecutiveNumbersSum(int n) {
-        int k=(int)sqrt(2*n+0.25)-0.5+1;
+        //n=x+1+x+2+..x+k
+        //n=xk+(k+1)*k/2
+        
+        //n/k-(k+1)/2=x>=0
+        
+        //n/k>=(k+1)/2
+        //2n>=k**2+k+1/4-1/4
+        //2n+1/4>=(k+1/2)**2
+        //k<=(2n+1/4)**0.5-0.5
         int out=0;
-        for(int i=1;i<=k;i++)
+        int v=(int)(sqrt(2*n+0.25)+0.5);
+        for(int k=1;k<v;k++)
         {
-            int val=n-(int)(i*(i+1)/2);
-            if(val%i==0)out++;
+            int val=n-(int)((k+1)*k/2);
+            if(val%k==0)out++;
         }
         return out;
     }
