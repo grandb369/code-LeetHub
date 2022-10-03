5722aa68703b550e423e6dcd0c90d362c09a4017
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2022-10-03 23:07:21 +0800

    Time: 346 ms (28.88%), Space: 95.5 MB (50.05%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     int minCost(string col, vector<int>& nums) {
         int out=0;
-        int su=nums[0];
-        vector<int>stack{nums[0]};
+        int cur=nums[0];
         char pre=col[0];
         for(int i=1;i<col.size();i++)
         {
-            su+=nums[i];
             if(pre!=col[i])
             {
-                out+=stack[0];
-                stack.clear();
+                cur=nums[i];
+            }
+            else
+            {
+                if (cur>nums[i])
+                {
+                    out+=nums[i];
+                }
+                else
+                {
+                    out+=cur;
+                    cur=nums[i];
+                }
             }
             pre=col[i];
-            while(stack.size()>0 && stack.back()<nums[i])
-            {
-                stack.pop_back();
-            }
-            stack.push_back(nums[i]);
         }
-        out+=stack[0];
-        return su-out;
+        return out;
     }
 };
