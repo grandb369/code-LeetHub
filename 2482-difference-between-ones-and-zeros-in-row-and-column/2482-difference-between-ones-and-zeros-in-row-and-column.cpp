bf5ebf3bb32b896a7045e4c8b4ac8bf4540cbaaa
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2023-12-14 14:30:38 +0800

    Time: 181 ms (87.98%), Space: 114.7 MB (98.60%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& mat) {
-        map<int,int>row;
-        map<int,int>col;
+        int r=0;
+        int c=0;
         int n=mat.size();
         int m=mat[0].size();
         int i,j;
         for(i=0;i<n;i++){
             for(j=0;j<m;j++){
                 int val=mat[i][j]==1?1:-1;
-                row[i]+=val;
-                col[j]+=val;
+                if(i==0 || j==0)mat[i][j]=0;
+                if(i==0 && j==0){
+                    r+=val;
+                    c+=val;
+                }
+                else if(i==0){
+                    r+=val;
+                    mat[0][j]+=val;
+                }
+                else if(j==0){
+                    c+=val;
+                    mat[i][0]+=val;
+                }
+                else{
+                    mat[i][0]+=val;
+                    mat[0][j]+=val;
+                }
             }
         }
-        for(i=0;i<n;i++){
-            for(j=0;j<m;j++){
-                mat[i][j]=row[i]+col[j];
+        for(i=n-1;i>=0;i--){
+            for(j=m-1;j>=0;j--){
+                if(i==0 && j==0){
+                    mat[0][0]=r+c;
+                }
+                else if(i==0){
+                    mat[i][j]+=r;
+                }
+                else if(j==0){
+                    mat[i][j]+=c;
+                }
+                else{
+                    mat[i][j]=mat[i][0]+mat[0][j];
+                }
             }
         }
         return mat;
