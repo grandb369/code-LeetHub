0f02c7e23a2896191ffb1a6cffbb598127aa6d3f
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2023-12-20 23:11:40 +0800

    Time: 12 ms (59.66%), Space: 47.1 MB (29.77%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(),prices.end());
-        return prices[0]+prices[1]<=money?money-prices[0]-prices[1]:money;
+        int v1,v2=-1;
+        for(int v:prices){
+            if(v1==-1)v1=v;
+            else if(v2==-1){
+                if(v<v1){
+                    v2=v1;
+                    v1=v;
+                }
+                else v2=v;
+            }
+            else{
+                if(v1>=v){
+                    v2=v1;
+                    v1=v;
+                }
+                else if(v2>v)v2=v;
+            }
+        }
+        cout<<v1<<','<<v2<<','<<money<<endl;
+        cout<<money-v1-v2<<endl;
+        cout<<(v1+v2<=money?money-v1-v2:money)<<endl;
+        return v1+v2<=money?money-v1-v2:money;
     }
 };
