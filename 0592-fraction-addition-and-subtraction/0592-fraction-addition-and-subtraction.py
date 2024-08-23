8ecb1c4d38d683671c1128f37cd4de9d4c103d91
Anthony LAM <42032578+grandb369@users.noreply.github.com>
2024-08-23 09:38:37 +0800

    Time: 38 ms (43.02%), Space: 16.4 MB (89.53%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -0,0 +1,30 @@
+import re
+
+
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        num = 0
+        denom = 1
+
+        # separate expression into signed numbers
+        nums = re.split("/|(?=[-+])", expression)
+        nums = list(filter(None, nums))
+
+        for i in range(0, len(nums), 2):
+            curr_num = int(nums[i])
+            curr_denom = int(nums[i + 1])
+
+            num = num * curr_denom + curr_num * denom
+            denom = denom * curr_denom
+
+        gcd = abs(self._find_gcd(num, denom))
+
+        num //= gcd
+        denom //= gcd
+
+        return str(num) + "/" + str(denom)
+
+    def _find_gcd(self, a: int, b: int) -> int:
+        if a == 0:
+            return b
+        return self._find_gcd(b % a, a)
